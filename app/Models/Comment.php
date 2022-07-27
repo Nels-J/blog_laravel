f667ed79c1aacb9c0a996201886761ace2fc78f2
NelsonJ <jesus.nels@gmail.com>
2022-07-27 16:41:47 +0200

    feat(db comments): added relationship Belongs to posts

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the post that owns the comment.
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
 }
