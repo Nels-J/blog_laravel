7fbc08a69664ea0ea01905a33be61b780b18a0e8
NelsonJ <jesus.nels@gmail.com>
2022-07-28 16:39:11 +0200

    refactor(db comments): added conditional statement in the CommentFactory
    Manage comments from authentified user rather than a guest user

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,10 +18,40 @@ class CommentFactory extends Factory
      */
     public function definition()
     {
-        return [
+        /*
+         * return [
+         *     'content' => fake()->text(2000),
+         *     'pseudo' => fake()->name(),
+         *     'email' => fake()->safeEmail(),
+         *     'post_id' => Post::inRandomOrder()->first(),  //query the post table to find an id to add it to the comment
+         *     'user_id' => User::inRandomOrder()->first(),  //query the user table to find an id to add it to the comment
+         *
+         * ];
+         */
+
+        // Refactor
+
+        $commentArray = [
+            'post_id' => Post::inRandomOrder()->first(),  //query the post table to find an id to add it to the comment
             'content' => fake()->text(2000),
-            'pseudo' => fake()->name(),
-            'email' => fake()->safeEmail(),
         ];
+
+            /* how to manage the 2 possibles cases in random way:
+             * A/ user non authentified
+             * B/ user authentified
+             */
+
+        $randomNumber = rand(0,1);
+
+        if( $randomNumber <= 0.3 ) {            // case A: if the random number is higher or equal than 0.3
+
+            $commentArray['user_id'] = User::inRandomOrder()->first(); //query the user table to find an id to add it to the comment
+
+        } else {            // case B: if the random number is higher than 0.3
+            $commentArray['pseudo'] = fake()->name();
+            $commentArray['email'] = fake()->safeEmail();
+        }
+
+        return $commentArray;
     }
 }
